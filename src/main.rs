cd65afe6a6a5e4872fdeb6976044fa7c419a9a03
SFlanag1 <steve.m.flanagan@t-mobile.com>
2021-08-12 18:01:01 -0500

    fixes

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,22 @@ fn main() {
 }
 
 fn run() -> Result<()> {
-    println!("Hello, world!");
-    let to_find = b'.';
-    let mut buf = vec![0; 4096];
+    // println!("Hello, world!");
+    let to_find = b';';
+    let mut buf = [0; 64*4096];
     let mut hm: HashMap<u64, u64> = HashMap::new();
     let mut line_count = 0;
     let mut curr_char = 0;
     for f in std::env::args().skip(1) {
         let path = PathBuf::from(f);
-        println!("reading file {}", &path.display());
+        // println!("reading file {}", &path.display());
         let mut rdr = BufReader::new(crate::decomp::open_comp_file(&path)?);
 
+        loop {
         let len = rdr.read(&mut buf[..])?;
+        if len == 0 {
+            break;
+        }
 
         buf[0..len].iter().for_each(|b|{
             if *b == to_find {
@@ -42,25 +46,12 @@ fn run() -> Result<()> {
                 curr_char = 0;
             }
         });
-
-        // for i in 0..len {
-        //     let b = buf[i];
-        //     if b == to_find {
-        //         curr_char += 1;
-        //     } else if b == b'\n' || b == b'\r' {
-        //         line_count += 1;
-        //         if let Some(x) = hm.get_mut(&curr_char) {
-        //             *x += 1;
-        //         } else {
-        //             hm.insert(curr_char, 1);
-        //         }               
-        //         curr_char = 0;
-        //     }
-        // }
+        }
     }
 
-    //let cc_vec = hm.iter().map(|(k,v| (k,v)).collect::Vec<u64,u64>();
-    for (k, v) in hm {
+    let mut cc_vec: Vec<(u64,u64)> = hm.iter().map(|(k,v)| (*k,*v)).collect();
+    cc_vec.sort_by(|a,b| b.1.partial_cmp(&a.1).unwrap());
+    for (k, v) in cc_vec {
         println!("{}  at  {}", k, v);
     }
     println!("line count: {}", line_count);
