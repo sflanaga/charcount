c8acee1072631697f919cb17b3bda3b8ae96373b
Steve Flanagan <stevemflanagan@gmail.com>
2021-08-17 11:10:32 -0500

    tic really working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, io::{BufRead, BufReader, Read}, path::PathBuf, sync::atomic::AtomicUsize, time::{Duration, Instant}};
+use std::{collections::HashMap, io::{BufRead, BufReader, Read}, path::PathBuf, rc::Rc, sync::{Arc, atomic::{AtomicUsize, Ordering}}, time::{Duration, Instant}};
 
 use anyhow::Result;
 
@@ -13,35 +13,47 @@ fn main() {
 
 fn run() -> Result<()> {
     // println!("Hello, world!");
-    let args:Vec<String> = std::env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
     if args.len() < 3 {
         eprintln!("usage: charcounts <character> file1 [file2 .. fileN]");
         std::process::exit(1);
     }
     let to_find = args.get(1).unwrap().as_bytes()[0];
-    let mut buf = [0; 64*4096];
+    let mut buf = [0; 64 * 4096];
     let mut hm: HashMap<u64, u64> = HashMap::new();
     let mut line_count = 0;
     let mut curr_char = 0;
 
-    let mut bytes = AtomicUsize::new(0);
-
-    let ticker = std::thread::spawn(move ||{
+    let mut bytes = Arc::new(AtomicUsize::new(1));
+    let mut t_bytes = bytes.clone();
+    let ticker = std::thread::spawn(move || {
         let start_f = Instant::now();
-
+        let mut last_bytes = 0usize;
+        let mut last_dur = start_f.elapsed();
         loop {
-            std::thread::sleep(Duration::from_secs(1));
+            std::thread::sleep(Duration::from_millis(10));
 
-            let total_bytes = bytes.load(std::sync::atomic::Ordering::Relaxed);
-            
+            let total_bytes = t_bytes.load(std::sync::atomic::Ordering::Relaxed);
+
             if total_bytes == 0 {
                 return;
             }
-
             let elapsed = start_f.elapsed();
-            let sec: f64 = (elapsed.as_secs() as f64) + (elapsed.subsec_nanos() as f64 / 1_000_000_000.0);
-            let rate = (total_bytes as f64 / sec) as usize;
-            eprintln!("bytes: rate {}/s  total {}                          \r", mem_metric_digit(rate,4), mem_metric_digit(total_bytes,4));
+
+            let d_bytes = total_bytes - last_bytes;
+            let d_dur = elapsed - last_dur;
+
+            let sec: f64 =
+                (d_dur.as_secs() as f64) + (d_dur.subsec_nanos() as f64 / 1_000_000_000.0);
+            let rate = (d_bytes as f64 / sec) as usize;
+            eprintln!(
+                "bytes: rate {}/s  total {}                          \r",
+                mem_metric_digit(rate, 4),
+                mem_metric_digit(total_bytes, 4)
+            );
+
+            last_bytes = total_bytes;
+            last_dur = elapsed;
         }
     });
 
@@ -51,36 +63,38 @@ fn run() -> Result<()> {
         let mut rdr = BufReader::new(crate::decomp::open_comp_file(&path)?);
 
         loop {
-        let len = rdr.read(&mut buf[..])?;
-        if len == 0 {
-            break;
-        }
-
-        buf[0..len].iter().for_each(|b|{
-            if *b == to_find {
-                curr_char += 1;
-            } else if *b == b'\n' || *b == b'\r' {
-                line_count += 1;
-                if let Some(x) = hm.get_mut(&curr_char) {
-                    *x += 1;
-                } else {
-                    hm.insert(curr_char, 1);
-                }               
-                curr_char = 0;
+            let len = rdr.read(&mut buf[..])?;
+            if len == 0 {
+                break;
             }
-        });
+
+            buf[0..len].iter().for_each(|b| {
+                if *b == to_find {
+                    curr_char += 1;
+                } else if *b == b'\n' || *b == b'\r' {
+                    line_count += 1;
+                    if let Some(x) = hm.get_mut(&curr_char) {
+                        *x += 1;
+                    } else {
+                        hm.insert(curr_char, 1);
+                    }
+                    curr_char = 0;
+                }
+            });
+            std::thread::sleep(Duration::from_millis(1));
+            bytes.fetch_add(len, Ordering::Relaxed);
         }
     }
 
-    let mut cc_vec: Vec<(u64,u64)> = hm.iter().map(|(k,v)| (*k,*v)).collect();
-    cc_vec.sort_by(|a,b| b.1.partial_cmp(&a.1).unwrap());
+    let mut cc_vec: Vec<(u64, u64)> = hm.iter().map(|(k, v)| (*k, *v)).collect();
+    cc_vec.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
     for (k, v) in cc_vec {
         println!("{}  at  {}", k, v);
     }
     println!("line count: {}", line_count);
+
     Ok(())
 }
-
 
 fn mem_metric<'a>(v: usize) -> (f64, &'a str) {
     const METRIC: [&str; 8] = ["B ", "KB", "MB", "GB", "TB", "PB", "EB", "ZB"];
@@ -122,9 +136,8 @@ fn sig_dig(v: f64, digits: usize) -> String {
 
 pub fn mem_metric_digit(v: usize, sig: usize) -> String {
     if v == 0 || v > std::usize::MAX / 2 {
-        return format!("{:>width$}", "unknown", width = sig + 3);
+        return format!("{:>width$}", "0", width = sig + 3);
     }
     let vt = mem_metric(v);
-    format!("{:>width$} {}", sig_dig(vt.0, sig), vt.1, width = sig + 1, )
+    format!("{:>width$} {}", sig_dig(vt.0, sig), vt.1, width = sig + 1,)
 }
-
